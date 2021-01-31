8e43b612417356b2a3ec2766065604bf12bb0f17
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-01-31 11:58:24 +0100

    initial commit

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,10 @@
+import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+
+@WebSocketGateway()
+export class ChatGateway {
+  @SubscribeMessage('message')
+  handleChatEvent(@MessageBody() data: string): string {
+    console.log(data);
+    return data + ' Hello';
+  }
+}
