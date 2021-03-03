2d3c72e4072ee531d3af9050f0009a9632b26a5e
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-03-03 09:34:01 +0100

    postgres starting setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/api/chat.module';
+import { DatabaseModule } from './chat/infrastructure/data-source/postgres/database.module';
+import Joi from '@hapi/joi';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ChatModule],
+  imports: [
+    ChatModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/chat/infrastructure/data-source/postgres/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ChatClientEntity } from '../entities/client.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [
+          ChatClientEntity
+        ],
+        synchronize: true, //false in production environment
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
